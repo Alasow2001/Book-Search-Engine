afa1d626baf7c2aef3894fda2981b00d370cd2a6
Ahmed Alasow <a.alasow01@gmail.com>
2023-03-07 13:04:35 +0000

    Refactored login and signup page to make use of the useMutation methods

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,7 +2,10 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+// import { loginUser } from '../utils/API';
+
+import { useMutation } from '@apollo/client'
+import {LOGIN_USER} from '../utils/mutations'
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
@@ -26,7 +29,7 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const response = await useMutation(userFormData);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
